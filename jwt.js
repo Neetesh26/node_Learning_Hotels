e7b833fb7df0701736697fe612b503700e4ac3de
Neetesh Prajapati <139002314+Neetesh26@users.noreply.github.com>
2025-01-20 00:20:14 +0530

    jwt

---
+++ jwt.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const jwtMiddleware = (req, res, next) => {
+  // extract token from headers
+  const token = req.headers.authorization.split(" ")[1];
+  if (!token) return res.status(401).json({ error: "unauthorized" });
+
+  try {
+    //    verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // attatch user info to object
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ error: "invalid token" });
+  }
+};
+
+// generate token --->
+const generatetoken = (userdata) => {
+  return jwt.sign(userdata, process.env.JWT_SECRET);
+};
+
+module.exports = { jwtMiddleware, generatetoken };
--- routes/personsroutes.js
+++ routes/personsroutes.js
@@ -1,65 +1,64 @@
 const express = require("express");
-const router = express.Router()
-
+const router = express.Router();
+const { jwtMiddleware, generatetoken } = require('../jwt');
 
 //model =--> table
 const person = require("../models/persons");
 
-
 router.post("/signup", async function (req, res) {
-    try {
-      // res.send("data is saved");
-      const data = req.body;
-  
-      const newPerson = new person(data);
-      // newPerson.name = data.name
-      // newPerson.age = data.age
-      // newPerson.work = data.work
-      // newPerson.mobile = data.mobile
-      // newPerson.email = data.email
-      // newPerson.address = data.address
-      // newPerson.salary = data.salary
-  
-      const datasaved = await newPerson.save();
-      console.log("data saved");
-      res.status(200).json(datasaved);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: "internal server error" });
-    }
-  });
-  
-  
-  //data find all user--->
-  router.get('/' ,async (req ,res)=>{
-      try {
-          const data = await person.find()
-          console.log("data fetched");
-          res.status(200).json(data);
-      } catch (error) {
-          console.log(error)
-          res.status(500).json({error: "internal server error"})
-      }
-  })
-  
-  router.get('/:worktype', async (req ,res)=>{
-    try {
-      const worktype = req.params.worktype;
-      if(worktype =='chef' , worktype == 'waiter'){
-  
-        const response =await person.find({work:worktype})
-        console.log('response fetched');
-        res.status(200).json(response)
-        
-      }
-      else{
-        res.status(404).json({error: "invalid page"})
-  
-      }
-    } catch (error) {
-      console.log(error)
-      res.status(500).json({error: "internal server error"})
-    }
-  })
+  try {
+    // res.send("data is saved");
+    const data = req.body;
 
-  module.exports = router
+    const newPerson = new person(data);
+    // newPerson.name = data.name
+    // newPerson.age = data.age
+    // newPerson.work = data.work
+    // newPerson.mobile = data.mobile
+    // newPerson.email = data.email
+    // newPerson.address = data.address
+    // newPerson.salary = data.salary
+
+    const datasaved = await newPerson.save();
+    console.log("data saved");
+
+    // const token = generatetoken(datasaved.username)
+    // console.log("token is : ", token);
+
+    // res.status(200).json({ datasaved: datasaved , token: token});
+    res.status(200).json({ datasaved});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "internal server error" });
+  }
+});
+
+//data find all user--->
+router.get("/", async (req, res) => {
+  try {
+    const data = await person.find();
+    console.log("data fetched");
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "internal server error" });
+  }
+});
+
+router.get("/:worktype", async (req, res) => {
+  try {
+    const worktype = req.params.worktype;
+    if ((worktype == "chef", worktype == "waiter")) {
+      const response = await person.find({ work: worktype });
+      console.log("response fetched");
+      res.status(200).json(response);
+    } else {
+      res.status(404).json({ error: "invalid page" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "internal server error" });
+  }
+});
+
+module.exports = router;
